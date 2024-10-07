0890d90ab7486ad74de28333ec071600ec2098f1
Jaime García <jaime.garcia@iesdonana.org>
2024-10-07 19:06:59 +0200

    Los inputs no llegan

---
+++ auxiliar.php
@@ -0,0 +1,95 @@
+<?php
+
+function agregar_error($par, $mensaje, &$errores)
+{
+    if (!isset($errores[$par])) {
+        $errores[$par] = [];
+    }
+    $errores[$par][] = $mensaje;
+}
+
+function obtener_parametro($par, $mensaje, &$errores)
+{
+    if (!isset($_GET[$par])) {
+        agregar_error($par, $mensaje, $errores);
+        return null;
+    }
+    return trim($_GET[$par]);
+}
+
+function comprobar_no_vacio($cadena, $par, $mensaje, &$errores)
+{
+    if ($cadena == '') {
+        agregar_error($par, $mensaje, $errores);
+    }
+}
+
+function hay_errores($errores)
+{
+    return !empty($errores);
+}
+
+function no_hay_errores($errores, $par)
+{
+    return !isset($errores[$par]) || empty($errores[$par]);
+}
+
+function comprobar_isbn(&$errores)
+{
+    $isbn = obtener_parametro('isbn', "Falta el ISBN.", $errores);
+    if (no_hay_errores($errores, 'isbn')) {
+        comprobar_no_vacio($isbn, 'isbn', "El ISBN es obligatorio.", $errores);
+    }
+
+    return $isbn;
+}
+
+function comprobar_titulo(&$errores)
+{
+    $titulo = obtener_parametro('titulo', "Falta el título.", $errores);
+    if (no_hay_errores($errores, 'titulo')) {
+        comprobar_no_vacio($titulo, 'titulo', "El título es obligatorio.", $errores);
+    }
+
+    return $titulo;
+}
+
+function comprobar_id(&$errores)
+{
+    $id = obtener_parametro('id', "Falta el ID del usuario.", $errores);
+    if (no_hay_errores($errores, 'id')) {
+        comprobar_no_vacio($id, 'id', "El ID del usuarioes obligatorio.", $errores);
+    }
+
+    return $id;
+}
+
+function comprobar_nombre(&$errores)
+{
+    $nombre = obtener_parametro('nombre', "Falta el nombre del usuario.", $errores);
+    if (no_hay_errores($errores, 'nombre')) {
+        comprobar_no_vacio($nombre, 'nombre', "El nombre del usuario es obligatorio.", $errores);
+    }
+
+    return $nombre;
+}
+
+function mostrar_mensajes_error($errores)
+{
+    foreach ($errores as $mensajes) {
+        foreach ($mensajes as $mensaje) { ?>
+            <h3><?= $mensaje ?></h3><?php
+        }
+    }
+}
+
+function calcula_devolucion($today)
+{
+    return ($today->add(new DateInterval('P30D')))->format('d/m/y');
+}
+
+function mostrar_resultado($isbn, $titulo, $id, $nombre, $fecha)
+{ ?>
+<p>El ejemplar de <?= $titulo ?> con ISBN <?= $isbn ?> ha sido sacado por el usuario <?= $nombre ?> con ID <?= $id ?>.</p>
+<p>Tiene hasta el día <?=$fecha?> para devolverlo.</p><?php
+}
---
+++ biblioteca.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Préstamo</title>
+</head>
+
+<body>
+    <?php
+    require 'auxiliar.php';
+
+    $errores = [];
+
+    $today = new DateTimeImmutable();
+    $isbn = comprobar_isbn($errores);
+    $titulo = comprobar_titulo($errores);
+    $id = comprobar_id($errores);
+    $nombre = comprobar_nombre($errores);
+
+    if (hay_errores($errores)) {
+        mostrar_mensajes_error($errores);
+    } else {
+        $fecha = calcula_devolucion($today);
+        mostrar_resultado($isbn, $titulo, $id, $nombre, $fecha);
+    }
+
+    ?>
+    <a href="index.html"><button>Volver</button></a>
+</body>
+
+</html>
